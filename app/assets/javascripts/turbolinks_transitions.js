cfd96d4e620e514f8b7dbe9f212ecc76fd37b6a8
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-23 18:50:01 -0500

    removed js file

--- app/assets/javascripts/turbolinks_transitions.js
+++
@@ -1,8 +0,0 @@
-// turbolinks_transitions.js
-
-document.addEventListener('page:change', function() {
-        document.getElementById('primary-content').className += 'animated fadeIn';
-});
-document.addEventListener('page:fetch', function() {
-        document.getElementById('primary-content').className += 'animated fadeOut';
-});
