64ced1ec14db9d9104f9ad643d328f828affbe92
Shawn David Broukhim <shawn.brouk@gmail.com>
2014-02-27 21:56:39 -0500

    more2 complete

---
+++ app/controllers/concerns/more2_controller.rb
@@ -0,0 +1,12 @@
+# more2_controller.rb
+class More2Controller < ApplicationController
+
+  include ColorHelper
+
+  def index
+  end
+
+  private
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ColorPage::Application.routes.draw do
 
   root 'welcome#index'
   get 'more', to: 'more#index'
+  get 'more2', to: 'more2#index'
 
  
 end
